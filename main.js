4c9229563183dcccf2208d58d39ac839b2cfd11a
Lin Lexiao <2623459403@qq.com>
2023-01-09 11:05:42 +0800

    支持多token

--- main.js
+++ main.js
@@ -3,135 +3,196 @@ const WebSocket = require('ws')
 const ZSTDDecoder = require("./zstd.js")
 const WS_URL = "wss://paint.yurzhang.com/api/ws";
 const data = require("./data")
-let token = null
+
 const fs = require('fs')
 fs.readFile('./token.txt', (err, data) => {
-    if (err) {
-        console.error(err)
-        return
+  if (err) {
+      console.error(err)
+      return
+  }
+  // data 是二进制类型，需要转换成字符串
+  console.log(data.toString())
+  tokens = data.toString().split(/\r?\n/);
+  if(tokens == 0){
+    console.alert("[ERROR]No Token!")
+    return;
+  }
+  console.log("[INFO]%d token(s) read.",tokens.length);
+  console.log("[INFO]try create main ws(%s).",tokens[0].substring(0,6));
+  createWs(tokens[0],true)
+  for(let i=1;i<tokens.length;i++){
+    if(tokens[i]==0){
+      console.log("[WARNING] token(%d)=\"%s\" skipped",i+1,tokens[i]);
+      continue;
     }
-    // data 是二进制类型，需要转换成字符串
-    console.log(data.toString())
-    token = data.toString();
-    connectWs();
+    console.log("[INFO]try create ws(%s).",tokens[i].substring(0,6));
+    createWs(tokens[i], false);
+  }
+  setInterval(maintain,508)
 })
 
 const W = 1000;
 const H = 600;
-
-const startX = 50
-const startY = 400;
+let wsList = []
+const startX = 20
+const startY = 480;
 const picY = data.length;
 const picX = data[0].length;
 
 //console.log("x,y",picX,picY)
 
-Myboard = Array.from(Array(H), () => new Array(W));
+const board = Array.from(Array(H), () => new Array(W));
+//const boardState = new Array(picY).fill(0).map(() => new Array(picX).fill(0));
+//const taskQueue = new Array()
+
+function coloreq(a,b){
+  return a[0]==b[0] && a[1]==b[1] && a[2]==b[2];
+}
 // console.log(Myboard)
 function update(a){
     //console.log("update",a.x,a.y);
-    Myboard[a.y][a.x]=a.c;
+    // if(startY<=a.y && a.y<startY+picY &&
+    //   startX<=a.x && a.x<startX+picX){
+    //   realX = a.x-startX;
+    //   realY = a.y-startY;
+    //   console.log("real",realX,realY);
+    //   if(!coloreq(a.c,data[realY][realX]) && boardState[realY][realX]==0){
+    //     taskQueue.push([a.y,a.x,a.c]);
+    //     boardState[realY][realX]=1;
+    //   }
+    // }
+    board[a.y][a.x]=a.c;
 }
 
 let allOk=false;
-let ws = null;
-const connectWs = () => {
 
-  if (token === null) {
-    return;
-  }
-
+function createWs(token,isMain){
+  let ws = null;
   try {
     ws = new WebSocket(WS_URL);
   } catch (e) {
-    console.log(e)
-    //alert("无法连接 WebSocket 服务器！");
+    console.alert("[ERROR]无法连接 WebSocket 服务器！");
     return;
   }
-
   ws.binaryType = "arraybuffer";
 
   ws.onopen = () => {
     const hex_token = token.replaceAll("-", "");
     let token_msg = [0xff];
+    console.log("[INFO]token(%s) send auth",token.substring(0,6));
     for (let i = 0; i < 16; ++ i) {
       const byte = parseInt(hex_token.slice(i * 2, i * 2 + 2), 16);
       token_msg.push(byte);
     }
     ws.send(new Uint8Array(token_msg));
   };
-
-  ws.onmessage = (event) => {
-    const raw_data = new Uint8Array(event.data);
-    const [type, data] = [raw_data[0], raw_data.slice(1)];
-
-    switch (type) {
-      case 0xfc: { // Auth success
-        console.log("Auth Success");
-        ws.send(new Uint8Array([0xf9]));
-        break;
-      }
-      case 0xfb: { // Get board
-        const decoder = new ZSTDDecoder();
-
-        decoder.init()
-          .then(() => {
-            const board = decoder.decode(data, 1800000);
-
-            if (board.length !== 1800000) {
-              console.log("Len:", board.length);
-              alert("画板损坏！请联系管理员！");
-            }
-
-            let idx = 0;
-            for (let x = 0; x < W; ++ x) {
-              for (let y = 0; y < H; ++ y) {
-                update({ y, x, c: board.slice(idx, idx + 3) },0);
-                idx += 3;
-              }
-            }
-
-            allOk = true;
-          });
-        break;
-      }
-      case 0xfa: { // Update
-        for (let i = 0; i < data.length; i += 7) {
-          const x = data[i + 1] * 256 + data[i];
-          const y = data[i + 3] * 256 + data[i + 2];
-          const c = data.slice(i + 4, i + 7);
-          update({ x, y, c },1);
+  if(isMain){
+    ws.onmessage = (event) => {
+      const raw_data = new Uint8Array(event.data);
+      const [type, data] = [raw_data[0], raw_data.slice(1)];
+      switch (type) {
+        case 0xfc: { // Auth success
+          console.log("[INFO]main token (%s) success",token.substring(0,6));
+          ws.send(new Uint8Array([0xf9]));
+          wsList.push(ws)
+          break;
         }
-        break;
+        case 0xfb: { // Get board
+          const decoder = new ZSTDDecoder();
+  
+          decoder.init()
+            .then(() => {
+              const board = decoder.decode(data, 1800000);
+  
+              if (board.length !== 1800000) {
+                console.log("Len:", board.length);
+                alert("画板损坏！请联系管理员！");
+              }
+  
+              let idx = 0;
+              for (let x = 0; x < W; ++ x) {
+                for (let y = 0; y < H; ++ y) {
+                  update({ y, x, c: board.slice(idx, idx + 3) },0);
+                  idx += 3;
+                }
+              }
+              console.log("[INFO]board loaded.");
+              allOk = true;
+            });
+          break;
+        }
+        case 0xfa: { // Update
+          for (let i = 0; i < data.length; i += 7) {
+            const x = data[i + 1] * 256 + data[i];
+            const y = data[i + 3] * 256 + data[i + 2];
+            const c = data.slice(i + 4, i + 7);
+            update({ x, y, c },1);
+          }
+          break;
+        }
       }
-    }
-  };
+    };
+  }
+  else{
+    ws.onmessage = (event) => {
+      const raw_data = new Uint8Array(event.data);
+      const [type, data] = [raw_data[0], raw_data.slice(1)];
+      switch (type) {
+        case 0xfc: { // Auth success
+          console.log("[INFO]token(%s) success",token.substring(0,6));
+          wsList.push(ws)
+          break;
+        }
+      }
+    };
+  }
+
 
   setInterval(() => { ws.send("p"); }, 10000);
-  setInterval(() => { maintain(ws) }, 510);
-};
+}
 
-function maintain(ws){
-    if (!allOk) {
-        return;
-    }
-    // console.log("Try mantian",ws);
-    function coloreq(a,b){
-        return a[0]==b[0] && a[1]==b[1] && a[2]==b[2];
-    }
+// unfinished
+// function multiMaintain(){
+//   // console.log("activate ws:%d",wsList.length)
+//   if(!allOk)return;
+//   let nowWs = 0;
+//   for(let i=0;i<taskQueue.length;i++){
+//     if(nowWs==wsList.length)return;
 
-    for(let i=startY;i<startY+picY;i++){
-        for(let j=startX;j<startX+picX;j++){
-            if(!coloreq(Myboard[i][j],data[i-startY][j-startX])){
-                console.log("Now matain",i,j)
-                change(ws,j,i,data[i-startY][j-startX]);
-                return;
-            }
-            else{
-                //console.log(i,j,"is the same(",board[i][j],"vs",data[i][j],")")
-            }
-        }
+//     let task = taskQueue.pop()
+//     change(wsList[nowWs],task[0],task[1],task[2]);
+//     console.log(task);
+//     boardState[task[0]-startY][task[1]-startX]=0;
+//     nowWs++;
+//   }
+// }
+
+function maintain(){
+  // console.log("[DEBUG]try maintain");
+  if (!allOk) {
+      return;
+  }
+  if(wsList.length==0)return;
+  // console.log("Try mantian",ws);
+  function coloreq(a,b){
+      return a[0]==b[0] && a[1]==b[1] && a[2]==b[2];
+  }
+  let nowWs = 0;
+  for(let i=startY;i<startY+picY;i++){
+    for(let j=startX;j<startX+picX;j++){
+      if(nowWs == wsList.length)return;
+      if(!coloreq(board[i][j],data[i-startY][j-startX])){
+        // console.log("Now matain",i,j)
+        console.log("[INFO]Now maintain (%d,%d) by token#%d",j,i,nowWs+1);
+        change(wsList[nowWs],j,i,data[i-startY][j-startX]);
+        nowWs++;
+      }
+      else{
+        //console.log(i,j,"is the same(",board[i][j],"vs",data[i][j],")")
+      }
     }
+  }
+
 }
 
 function change(ws,x,y,c){
@@ -147,4 +208,5 @@ function change(ws,x,y,c){
     ];
 
     ws.send(new Uint8Array(msg));
+    
 };
